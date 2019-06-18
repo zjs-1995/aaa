e77d9579e980907cb61f24acbbdf243455c0db2b
zjs <1164335025@qq.com>
2019-06-18 08:38:02 +0800

    sfsdgfg

--- git-project/src/com/itheima/git/FirstClass.java
+++ git-project/src/com/itheima/git/FirstClass.java
@@ -7,5 +7,6 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        System.out.println("hello git project");
     }
 }
